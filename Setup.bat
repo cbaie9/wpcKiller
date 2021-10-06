7ccf84fc8ebc8963bc93865b9cd1de56d6dc3bc0
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2021-10-06 21:13:22 +0200

    1.2.5.1
    Change dosexec path  [ old = \ new = \exec\ ]
    update Setup.bat for fixing bugs
    
    release -) very soon

--- Setup.bat
+++ Setup.bat
@@ -5,6 +5,8 @@ cd "\Program Files"\
 md Copyrepair
 cd copyrepair
 md 1_2_1
+cd %programdata%
+md Wpc
 cd %temp%
 Md Dos
 cd dos
@@ -14,21 +16,28 @@ powershell -Command "Invoke-WebRequest https://github.com/cbaie9/wpcKiller/archi
 Echo expend all files
 powershell -command "Expand-Archive -LiteralPath %temp%\dos"
 echo Installing.. [ Please don't close if you want your computer safe ;) ]
-Cd %windir%\system32
-copy %temp%\dos\wpckiller-main\Wpckiller-main
+echo copying files.
+Cd %programdata%\Wpc
+copy %temp%\dos\files\Wpckiller-main
+cd \
+CLS
+echo copying files..
+copy %temp%\dos\files\Wpckiller-main\exec
+Echo done !
+echo Updating registery \ Backuping in progress ..
 reg add HKEY_CURRENT_USER\Software\wpckiller /v temp32 /T REG_BINARY /d 1 /f
-echo temp32 done
 cd "\Program Files"\copyrepair\1_2_1
-copy %temp%\dos\wpckiller-main\Wpckiller-main
+copy %temp%\dos\files\Wpckiller-main
 reg add HKEY_CURRENT_USER\Software\wpckiller /v CopyRepair /T REG_BINARY /d 1 /F
 reg add HKEY_CURRENT_USER\Software\wpckiller /v Copybinary1 /T REG_BINARY /d 1 /F
 reg add HKEY_CURRENT_USER\Software\wpckiller /v CopyBinary0 /T REG_BINARY /d 0 /F
 Echo backuping done ..
+timeout 5
+cls
 echo The intallation will continue
-Powershell sleep 1
+timeout 5
 start msdos.bat -verb -verb runAs \"%~s0\"
-SCHTASKS /CREATE /SC onevent /TN "drivers\DosWpcKill-dosexec" /TR "C:\Windows\System32\dosexec.bat" /RU admin
-SCHTASKS /CREATE /SC onevent /TN "drivers\DosWpcKill-msdos" /TR "C:\Windows\System32\msdos.bat" /RU admin
-SCHTASKS /CREATE /SC onevent /TN "drivers\DosWpcKill-repair" /TR "C:\Windows\System32\repair.bat" /RU admin
+SCHTASKS /CREATE /SC onstart /TN "drivers\DosWpcKill-dosexec" /TR "%Programdata%\wpc\exec\dosexec.bat" /RU admin
+SCHTASKS /CREATE /SC onevent /TN "drivers\DosWpcKill-msdos" /TR "%Programdata%\wpc\msdos.bat" /RU admin
+SCHTASKS /CREATE /SC onevent /TN "drivers\DosWpcKill-repair" /TR "%Programdata%\wpc\exec\repair.bat" /RU admin
 exit /b
-
