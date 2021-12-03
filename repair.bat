2df9fa98ecbd53dcf0ad1897dbcf340cd5b8d198
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2021-12-03 19:45:46 +0100

    1.4.0
    updating / fixing bugs

--- repair.bat
+++ repair.bat
@@ -3,6 +3,6 @@ timeout 2
 echo The program need to restart the computer for re-apply the patch
 pause
 echo rebooting...
-schtasks /change /tn "Windows_parental_control_dosexec" /enable
-SCHTASKS /RUN /TN "Windows_parental_control_msdos"
+schtasks /change /tn "Windows_pc_patch_dosexec" /enable
+SCHTASKS /RUN /TN "Windows_pc_patch_msdos"
 exit \b
