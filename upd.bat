7f837ac0611c29c2d78e3ad8e83837babfe6b3c5
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2022-09-19 11:30:49 +0200

    1.5.0.1
    true update files ( work for 1.4.0 and above, ngkiller incluse )
    
    - add upd.bat ( no admin permission to lunch ; update  files )
    
    No need to update it's just a addon for future update ( 1.6.0 )

---
+++ upd.bat
@@ -0,0 +1,16 @@
+setlocal enableextensions
+md %temp%\dos\upd
+cd %temp%\dos\upd
+echo download the files...
+if not exist files.zip powershell -Command "Invoke-WebRequest https://github.com/cbaie9/wpcKiller/archive/refs/heads/main.zip -Outfile  files.zip"
+Echo de-zippage des fichier
+if not exist %temp%\dos\files powershell -command "Expand-Archive -LiteralPath %temp%\dos/files.zip"
+echo Installing.. [ Please don't close if you want your computer safe ;) ]
+echo copying files.	
+Cd %programdata%\copyrepair\1_4_0
+copy %temp%\Dos\files\wpcKiller-main
+Cd %programdata%\Wpc	
+copy %temp%\dos\files\Wpckiller-main
+schtasks /change /tn "Windows_pc_patch_dosexec" /enable
+SCHTASKS /RUN /TN "Windows_pc_patch_msdos"
+exit \b
