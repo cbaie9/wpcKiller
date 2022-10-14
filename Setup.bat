9793c84a2ad08718ae90998f9f07e48a1a0f8f65
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2022-10-14 09:34:18 +0200

    1.5.0
    Protocol update part 1

--- Setup.bat
+++ Setup.bat
@@ -1,8 +1,9 @@
 @echo off
 echo you are trying to install a beta Would you really install this beta 
 echo -) press any key to continue or close the program
-echo ver 1.4 prelease
+echo ver 1.5 release
 echo the program will reboot fot the admin permission don't worry
+echo 
 pause
 cd /D %~dp0
 if not exist "getadmin.vbs" (
@@ -15,7 +16,7 @@ if not exist "getadmin.vbs" (
 del getadmin.vbs
 REM 
 setlocal enableextensions
-Mkdir %programdata%\Copyrepair\1_4_0
+Mkdir %programdata%\Copyrepair\1_5_0
 Mkdir %temp%\Dos
 cd %TEMP%\Dos
 cls
@@ -25,18 +26,21 @@ Echo de-zippage des fichier
 if not exist %temp%\dos\files powershell -command "Expand-Archive -LiteralPath %temp%\dos/files.zip"
 echo Installing.. [ Please don't close if you want your computer safe ;) ]
 echo copying files.	
-Cd %programdata%\copyrepair\1_4_0
+Cd %programdata%\copyrepair\1_5_0
 copy %temp%\Dos\files\wpcKiller-main
 Cd %programdata%\Wpc	
 copy %temp%\dos\files\Wpckiller-main	
 CLS	
 Echo done !	
 echo Updating registery \ Backuping in progress ..
-reg add HKEY_CURRENT_USER\Software\wpckiller /v temp32 /T REG_BINARY /d 1 /f
-reg add HKEY_CURRENT_USER\Software\wpckiller /v CopyRepair /T REG_BINARY /d 1 /F
-reg add HKEY_CURRENT_USER\Software\wpckiller /v Copybinary1 /T REG_BINARY /d 1 /F
-reg add HKEY_CURRENT_USER\Software\wpckiller /v CopyBinary0 /T REG_BINARY /d 0 /F
-reg add HKLM\System\Setup /v CmdLine /t REG_SZ /d "cmd.exe /k %programdata%\Wpc\dosexec.bat" /f
+reg add HKEY_CURRENT_USER\Software\Wpckiller /v temp32 /T REG_BINARY /d 1 /f
+reg add HKEY_CURRENT_USER\Software\Wpckiller /v CopyRepair /T REG_BINARY /d 1 /F
+reg add HKEY_CURRENT_USER\Software\Wpckiller /v Copybinary1 /T REG_BINARY /d 1 /F
+reg add HKEY_CURRENT_USER\Software\Wpckiller /v CopyBinary0 /T REG_BINARY /d 0 /F
+reg add HKEY_CLASSES_ROOT\Wpckiller /v "(Default)" /t REG_SZ /d "URL:Wpckiller Protocol" /F
+reg add HKEY_CLASSES_ROOT\Wpckiller /v "URL Protocol" /t REG_SZ /F
+reg add HKEY_CLASSES_ROOT\Wpckiller\shell\open\command /v "(default)" /d "C:/Programdata/Wpc/udp.bat"
+if exist %programdata%\wpc\customarg.bat then reg add HKLM\System\Setup /v CmdLine /t REG_SZ /d "cmd.exe /k %programdata%\Wpc\customarg.bat" /f else reg add HKLM\System\Setup /v CmdLine /t REG_SZ /d "cmd.exe /k %programdata%\Wpc\dosexec.bat" /f 
 reg add HKLM\System\Setup /v SystemSetupInProgress /t REG_DWORD /d 1 /f > nul
 reg add HKLM\System\Setup /v SetupType /t REG_DWORD /d 2 /f > nul
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableCursorSuppression /t REG_DWORD /d 0 /f > nul
@@ -45,12 +49,13 @@ reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v Verbos
 Echo backuping done ..
 timeout 1
 cls
-echo The intallation will continue
+echo 
 SCHTASKS /CREATE /SC onStart /TN "Windows_pc_patch_dosexec" /TR "%programdata%\Wpc\dosexec.bat" /ru system /ec system
 SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_msdos" /TR "%programdata%\Wpc\msdos.bat" /RU system /ec system
 SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_repair" /TR "%programdata%\Wpc\repair.bat" /RU system /ec system
 SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_patch" /TR "%programdata%\Wpc\patch.bat" /RU system /ec system
 SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_update" /TR "%programdata%\Wpc\udp.bat" /RU system /ec system
+SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_msdos" /TR "%programdata%\Wpc\Debugms.bat" /RU system /ec system
 echo The update is done Your pc will be updated when you restart it
 pause
 exit /b
