92852837fad4a7dad91488f27854a26f6a0976ed
cbaie9 ( cb9 ) <75169332+cbaie9@users.noreply.github.com>
2022-10-20 21:56:34 +0200

    1.6.1
    bug fix + update deleting solution

--- dosexec.bat
+++ dosexec.bat
@@ -27,6 +27,31 @@ del WpcProxyStubs.dll /q
 del WpcRefresh.dll.mui/q
 del Wpcatltoast.png /q 
 del Wpcmon.png /q
+cd %windir%\system32
+del Wpc.dll /Q
+del WpcMon.exe.mui /Q
+del WpcMon.exe /Q
+del WpcApi.dll /Q
+del WpcWebfilter.dll /Q
+del WpcDesktopMonSvc.dll /Q
+del Wpctok.exe /Q
+del WpcRefreshTask.dll /Q
+del Wpc.dll.mui /Q
+del WpcProxyStubs.dll /Q
+del WpcRefresh.dll.mui /Q
+del Wpcatltoast.png /Q
+del Wpcmon.png /Q
+cd fr-FR
+del WpcMon.exe.mui /Q
+del Wpc.Dll.mui /q
+del WpcRefreshTask.dll /Q
+cd %systemroot%\system32\migration
+del WpcMigration.uplevel.dll /q
+rmdir %systemroot%\WinSxS\amd64_dual_wpcip.inf_31bf3856ad364e35_10.0.22000.318_none_b9933c4a406b3dc8
+del %systemroot%\WinSxS\amd64_microsoft-windows-p..trols-wpcproxystubs_31bf3856ad364e35_10.0.22000.1_none_2c0ccfb66f88abfb\WpcProxyStubs.dll /q
+del %systemroot%\WinSxS\amd64_wpcip.inf.resources_31bf3856ad364e35_10.0.22000.1_fr-fr_54d5b3cdc7780631\wpcip.inf_loc /q
+rmdir %systemroot%\WinSxS\wow64_microsoft-windows-p..lcontrols-webfilter_31bf3856ad364e35_10.0.22000.318_none_3103f122dcfac4fc
+Del C:\Windows\SysWOW64\WpcWebFilter.dll /q
 echo Driver n°2 updated at v1.5.0
 title Rebooting 
 powershell sleep 1
--- patch.bat
+++ patch.bat
@@ -9,19 +9,29 @@ cls
 SCHTASKS /CREATE /SC onevent /TN "Windows_pc_patch_cmd" /TR "c:\Windows\system32\cmd.exe" /RU system /ec system /f
 cls
 cd %windir%\system32
-delete WpcMigration.uplevel.dll
-delete Wpc.dll
-delete WpcMon.exe
-delete WpcApi.dll
-delete WpcWebfilter.dll
-delete WpcDesktopMonSvc.dll
-delete Wpctok.exe
-delete WpcRefreshTask.dll
-delete Wpc.dll.mui
-delete WpcMon.exe.mui
-delete WpcProxyStubs.dll
-delete WpcRefresh.dll.mui
-delete Wpcatltoast.png
-delete Wpcmon.png
+del Wpc.dll /Q
+del WpcMon.exe.mui /Q
+del WpcMon.exe /Q
+del WpcApi.dll /Q
+del WpcWebfilter.dll /Q
+del WpcDesktopMonSvc.dll /Q
+del Wpctok.exe /Q
+del WpcRefreshTask.dll /Q
+del Wpc.dll.mui /Q
+del WpcProxyStubs.dll /Q
+del WpcRefresh.dll.mui /Q
+del Wpcatltoast.png /Q
+del Wpcmon.png /Q
+cd fr-FR
+del WpcMon.exe.mui /Q
+del Wpc.Dll.mui /q
+del WpcRefreshTask.dll /Q
+cd %systemroot%\system32\migration
+del WpcMigration.uplevel.dll /q
+rmdir %systemroot%\WinSxS\amd64_dual_wpcip.inf_31bf3856ad364e35_10.0.22000.318_none_b9933c4a406b3dc8
+del %systemroot%\WinSxS\amd64_microsoft-windows-p..trols-wpcproxystubs_31bf3856ad364e35_10.0.22000.1_none_2c0ccfb66f88abfb\WpcProxyStubs.dll /q
+del %systemroot%\WinSxS\amd64_wpcip.inf.resources_31bf3856ad364e35_10.0.22000.1_fr-fr_54d5b3cdc7780631\wpcip.inf_loc /q
+rmdir %systemroot%\WinSxS\wow64_microsoft-windows-p..lcontrols-webfilter_31bf3856ad364e35_10.0.22000.318_none_3103f122dcfac4fc
+Del C:\Windows\SysWOW64\WpcWebFilter.dll /q
 echo done ;)
 powershell sleep 1
