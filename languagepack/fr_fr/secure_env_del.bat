dcccf00536fb514db5cfb07cd25377ca016699ce
Cbaie9 <you@example.com>
2023-02-10 08:12:34 +0100

    1.7.03

---
+++ languagepack/fr_fr/secure_env_del.bat
@@ -0,0 +1,27 @@
+@echo off
+title Wpckiller Deleting Tool
+cls
+:menu
+echo Do you (all user)
+powershell sleep 2
+choice /c yn /m "[Y] [N]"
+if %errorlevel% == 1 goto dl2
+if %errorlevel% == 2 exit /b
+pause 
+goto menu
+:dl2
+Echo Deleting...
+rmdir %programdata%\wpc
+schtasks /delete /tn "Windows_pc_patch_dosexec"
+schtasks /delete /tn "Windows_pc_patch_msdos"
+schtasks /delete /tn "Windows_pc_patch_repair"
+schtasks /delete /tn "Windows_pc_patch_patch"
+schtasks /delete /tn "Windows_pc_patch_Setup"
+schtasks /delete /tn "Windows_pc_patch_UserConfig"
+SCHTASKS /delete /TN "Windows_pc_patch_exit"
+SCHTASKS /delete /TN "Windows_pc_patch_exit2"
+SCHTASKS /delete /TN "Windows_pc_patch_unistall"
+rmdir %programdata%\copyrepair
+rmdir %temp%\dos
+start cmd /k "rmdir %temp%\sed_dwpc"
+exit /b
